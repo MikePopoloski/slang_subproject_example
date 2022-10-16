f97eae9966f263ca6220da00fc5301f86ca9ebdc
MikePopoloski <mike@popoloski.com>
2022-10-16 14:25:16 -0400

    Add example

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include "slang/driver/Driver.h"
+#include "slang/util/Version.h"
+
+using namespace slang;
+using namespace slang::driver;
+
+int main(int argc, char** argv) {
+    Driver driver;
+    driver.addStandardArgs();
+
+    std::optional<bool> showHelp;
+    std::optional<bool> showVersion;
+    driver.cmdLine.add("-h,--help", showHelp, "Display available options");
+    driver.cmdLine.add("--version", showVersion, "Display version information and exit");
+
+    if (!driver.parseCommandLine(argc, argv))
+        return 1;
+
+    if (showHelp == true) {
+        printf("%s\n", driver.cmdLine.getHelpText("slang SystemVerilog compiler").c_str());
+        return 0;
+    }
+
+    if (showVersion == true) {
+        printf("slang version %d.%d.%d+%s\n", VersionInfo::getMajor(),
+            VersionInfo::getMinor(), VersionInfo::getPatch(),
+            std::string(VersionInfo::getHash()).c_str());
+        return 0;
+    }
+
+    if (!driver.processOptions())
+        return 2;
+
+    bool ok = driver.parseAllSources();
+
+    auto compilation = driver.createCompilation();
+    ok &= driver.reportCompilation(*compilation, /* quiet */ false);
+
+    return ok ? 0 : 3;
+}
